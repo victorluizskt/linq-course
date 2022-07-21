df490936bba7b3d6d45376535248be29f4cb61f9
Victor Luiz <c0007043@vli-logistica.com.br>
2022-07-20 21:24:07 -0300

    Function in function

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FunctionInFunction(IsDog, "Cat"));
+            Console.ReadKey();
+        }
+
+        public static bool FunctionInFunction(Func<string, bool> predicate, string param1)
+        {
+            var predicateFunction = predicate(param1);
+            return predicateFunction;
+        }
+
+        public static bool IsDog(string param1)
+        {
+            return param1.Equals("Dog");
+        }
+    }
+}
